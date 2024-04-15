dc02a1143d65f63c744374878d8fab13b5ed4f2c
Alex Troia <brian.troia@icloud.com>
2024-04-15 13:04:58 +0200

    completato Scaffolding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div class="container-fluid text-center text-white">
+        <div class="row justify-content-center bg-dark">
+            <div class="col-12 col-md-4">
+                <h1>
+                    The Aulab Post
+                </h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
